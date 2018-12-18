6d21ca0ad5cdf63e999d895889300fe6816cedd6
Chad Evans <chad.evans@salesforce.com>
2018-12-17 22:03:04 -0500

    Update to functionality

--- force-app/main/default/aura/GoogleReCaptcha/GoogleReCaptchaController.js
+++ force-app/main/default/aura/GoogleReCaptcha/GoogleReCaptchaController.js
@@ -1,6 +1,7 @@
 ({
     doInit: function (component, event, helper) {
-        var vfOrigin = "https://customer-connect-9256-dev-ed--c.visualforce.com";
+        //example: "https://customer-connect-9256-dev-ed--c.visualforce.com";
+        var vfOrigin = component.get("v.vfurl");
         
         window.addEventListener("message", function (event) {
             if (event.origin !== vfOrigin) {
@@ -10,31 +11,11 @@
             
             if (event.data.action === "reCaptchaCallingAura") {
                 if (event.data.alohaResponseCAPTCHA === "NOK") {
-                    console.log("Failed to do the reCAPTCHA");
+                    component.set("v.valid", false);
                 } else if (event.data.alohaResponseCAPTCHA === "OK") {
-                    helper.sendEvent(component);
-                }
-            }
-            if (event.data.action === "auraCallingReCaptcha") {
-                if (event.data.alohaResponseCAPTCHA === "NOK") {
-                    console.log("Please do the reCAPTCHA before submit!");
-                } else if (event.data.alohaResponseCAPTCHA === "OK") {
-                    helper.acknowledgeSubmit(component);
+                    component.set("v.valid", true);
                 }
             }
         }, false);
-    },
-    
-    submitSomething: function (component, event, helper) {
-        var message = "auraCallingReCaptcha";
-        var vfOrigin = "https://customer-connect-9256-dev-ed--c.visualforce.com";
-        var vfWindow = component.find("vfFrame").getElement().contentWindow;
-        vfWindow.postMessage(
-            {
-                action: "auraCallingReCaptcha"
-            },
-            vfOrigin
-        );
-        
     }
 })
--- force-app/main/default/aura/GoogleReCaptchaApp/GoogleReCaptchaApp.app
+++ force-app/main/default/aura/GoogleReCaptchaApp/GoogleReCaptchaApp.app
@@ -1,3 +1,5 @@
 <aura:application >
-    <c:GoogleReCaptcha />
+    <c:GoogleReCaptcha sitekey="6LfXgYIUAAAAAG6JWAWN2XUzXvlpkN0hu8y6x1On"
+                       hosturl="https://customer-connect-9256-dev-ed.lightning.force.com"
+                       vfurl="https://customer-connect-9256-dev-ed--c.visualforce.com" />
 </aura:application>
